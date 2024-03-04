c66ed7df8f751331b8372ae80abdcc80d0b91416
ArsyWK <161662196+ArsyWK@users.noreply.github.com>
2024-03-04 04:12:41 -0800

    percobaan 3b.c

--- Unit 1/percobaan 3b.c
+++ Unit 1/percobaan 3b.c
@@ -2,6 +2,5 @@
 
 int main() {
   printf("Oh my, oh my God \n예상했어 나 \nI was really hoping that he will come through");
-  return 0;
-  /*output dihasilkan akan mengandung unsur enterdan fungsi dari \n adalah memberikan jarak 1 baris */
+  return 0;//memberi jarak satu baris
 }
