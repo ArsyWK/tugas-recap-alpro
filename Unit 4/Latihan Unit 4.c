c8961d44fd2b958b91c4449f52fd46e3add8c9d0
ArsyWK <161662196+ArsyWK@users.noreply.github.com>
2024-03-05 03:58:26 -0800

    Latihan Unit 4.c

--- Unit 4/Latihan Unit 4.c
+++ Unit 4/Latihan Unit 4.c
@@ -3,45 +3,28 @@
 int sisi;
 int menu;
 
-void keliling_Persegi(int a) {
+void keliling_persegi(int a) {
     int klp = 4 * a;
-    printf("%d\n", klp);
+    printf("Keliling Persegi: %d\n", klp);
 }
 
 void luas_persegi(int a) {
     int ss = a * a;
-    printf("%d\n", ss);
+    printf("Luas Persegi: %d\n", ss);
 }
 
 void volume_kubus(int a) {
     int vlb = a * a * a;
-    printf("%d\n", vlb);
+    printf("Volume Kubus: %d\n", vlb);
 }
 
 int main() {
-    printf("masukan menu 1.kelilingpersegi,2.luaspersegi,3.volumekubus\n");
-    scanf("%d", &menu);
-    switch(menu) {
-        case 1:
-            printf("masukan sisi:\n");
-            scanf("%d", &sisi);
-            keliling_Persegi(sisi);
-            break;
-            
-        case 2:
-            printf("masukan sisi:\n");
-            scanf("%d", &sisi);
-            luas_persegi(sisi);
-            break;
-
-        case 3:
-            printf("masukan sisi:\n");
-            scanf("%d", &sisi);
-            volume_kubus(sisi);
-            break;
-            
-        default:
-            break;
-    }
+    printf("Masukkan sisi: ");
+    scanf("%d", &sisi);
+    
+    keliling_persegi(sisi);
+    luas_persegi(sisi);
+    volume_kubus(sisi);
+    
     return 0;
 }
